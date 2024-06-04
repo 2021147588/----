41d3902176e628baad27b19fd463011e8b5a5e1c
2021147588 <dayen2001@naver.com>
2024-06-04 15:22:10 +0900

    init

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const port = 3000;
+
+// Static files middleware
+app.use('/simulate', express.static(path.join(__dirname, 'quickstart-web-master')));
+app.use('/', express.static(path.join(__dirname, 'main-page')));
+
+// Routes (optional)
+app.get('/simulate', (req, res) => {
+    res.sendFile(path.join(__dirname, 'quickstart-web-master', 'index.html'));
+});
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'main-page', 'index.html'));
+});
+
+// Start server
+app.listen(port, () => {
+    console.log(`Server running at http://localhost:${port}/`);
+});
